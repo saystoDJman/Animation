d720bd7c03fbee1ada9e0bc08e09038853da111c
Kv Sai Krishna <saikrishna.kelam@outlook.com>
2021-06-14 16:34:08 +0530

    Login

--- app/src/main/java/com/example/animation/LoginActivity.java
+++ app/src/main/java/com/example/animation/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.animation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button login;
+    EditText username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username = findViewById(R.id.etvUserName);
+        password = findViewById(R.id.etvPassword);
+        login = findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().trim().equals("Dhanush") && password.getText().toString().trim().equals("1234")){
+                    Intent intent =new Intent(getBaseContext(),MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Invalid Credentials",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
+
+
 }
